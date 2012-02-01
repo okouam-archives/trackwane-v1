085b111649ebfe445e478428b6d16f752f3e6c34
Olivier Kouame <olivier.kouame@gmail.com>
2012-02-01 06:25:18 -0800

    Added Capistrano deployment files

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+require 'capistrano/ext/multistage'
+require File.dirname(__FILE__) + '/boot'
+
+set :application, "geocms"
+set :repository,  "git@github.com:0-One/Gowane.git"
+set :scm, :git
+set :branch, :master
+set :stages, ["staging", "production"]
+
+set :default_stage, "staging"
+set :deploy_via, :remote_cache
+set :user, "deployment"
+set :ssh_options, { :forward_agent => true }
+set :rake, "/var/lib/gems/1.8/bin/rake"
+role :web, "galileo.codeifier.com"
+role :app, "galileo.codeifier.com"
+role :db,  "galileo.codeifier.com", :primary => true
+
+default_run_options[:pty] = true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+ 
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install --without test"
+  end
+end
+
+after 'deploy:update_code', 'bundler:bundle_new_release'
+
+after 'bundler:bundle_new_release' do
+  run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
+  run "cd #{release_path}; RAILS_ENV=production rake geocms:assets"
+end
