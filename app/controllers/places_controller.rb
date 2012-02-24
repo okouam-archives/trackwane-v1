98288b05574d4c1ef6a51e7ab721f13d57d4b2df
Olivier Kouame <olivier.kouame@gmail.com>
2012-02-24 12:28:47 +0000

    Added CRUD functionality for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,11 +2,29 @@ class PlacesController < ApplicationController
   before_filter :require_user
 
   def index
-    @places = session[:account].places
     respond_to do |format|
       format.html
-      format.json {render json: {success: true, results: @places}}
+      format.json do
+        @places = Account.find(session[:account_id]).places
+        render json: {success: true, results: @places}
+      end
     end
+  end
+
+  def create
+    changes = params.slice(*Place.column_names)
+    place = Place.new(changes)
+    place.account = Account.find(session[:account_id])
+    if place.save
+      render json: {success: true, results: [place.as_json]}
+    else
+      render json: {success: false}
+    end
+  end
+
+  def destroy
+    Place.find(params[:id]).destroy
+    render json: {success: true}
   end
 
 end
