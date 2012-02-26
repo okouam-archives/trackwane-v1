7b49173071dff916d180b4bb5ef7deeb4b544430
Olivier Kouame <olivier.kouame@gmail.com>
2012-02-26 16:55:39 +0000

    All

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120226120748) do
+ActiveRecord::Schema.define(:version => 20120226155342) do
 
   create_table "accounts", :force => true do |t|
     t.column "name", :string
